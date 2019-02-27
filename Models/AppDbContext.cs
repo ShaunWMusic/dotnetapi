08f4941d873ec187fe9f545ada00f62095b2646f
shaunwillis-SpectrumReach <shaun.willis@charter.com>
2019-02-26 19:42:41 -0600

    initial setup done, author model, controller added

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
+
+namespace LibraryApi.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Database=libraryapi;");
+        }
+
+        public DbSet<Author> Authors { get; set; }
+    }
+}
---
+++ Models/Author.cs
@@ -0,0 +1,11 @@
+using JsonApiDotNetCore.Models;
+
+namespace LibraryApi.Models
+{
+    public class Author : Identifiable
+    {
+        [Attr("first")] public string First { get; set; }
+
+        [Attr("last")] public string Last { get; set; }
+    }
+}
